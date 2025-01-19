862d20f8cadc60f3870ac6a070864eb7da3008df
Nikita-Tig <nikitatig2005@gmail.com>
2025-01-19 12:02:12 +0300

    JS done the funtction for adding cards and function to delete a card

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,45 @@
+const profileSection = document.querySelector(".profile");
+const addCardButton = profileSection.querySelector(".profile__add-button");
+
+const cardContainer = document.querySelector(".places__list");
+const cardTemplate = document.querySelector("#card-template").content;
+
+const cardsLinks = initialCards.map((element) => {
+  return element.link;
+});
+const cardsNames = initialCards.map((element) =>{
+  return element.name;
+});
+let i = 0
+
+// Функция создания карточки
+function addCard(link, name, findDeleteButton) {
+  const cardItem = cardTemplate.querySelector(".card").cloneNode(true);
+
+  cardItem.querySelector(".card__image").src = link;
+  cardItem.querySelector(".card__image").alt = name;
+  cardItem.querySelector(".card__title").textContent = name;
+  i = i + 1
+
+  cardContainer.append(cardItem);
+  findDeleteButton(cardItem);
+};
+
+// Функция удаления карточки
+function findDeleteButton(cardItem) {
+  const cardDeleteButton = cardItem.querySelector(".card__delete-button");
+  cardDeleteButton.addEventListener("click", deleteCard);
+
+  function deleteCard() {
+    cardItem.remove();
+  }
+}
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
 
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(() => {
+  addCard(cardsLinks[i], cardsNames[i], findDeleteButton)
+});
