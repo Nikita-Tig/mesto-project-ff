dd2db8c57401ddcfba0f32ac787c449823064694
Nikita-Tig <nikitatig2005@gmail.com>
2025-01-20 14:45:45 +0300

    Reworked the adding and deleting functions

--- scripts/index.js
+++ scripts/index.js
@@ -2,38 +2,31 @@
 const cardContainer = document.querySelector(".places__list");
 // Темплейт карточки
 const cardTemplate = document.querySelector("#card-template").content;
-const cardsLinks = initialCards.map((element) => {
-  return element.link;
-});
-const cardsNames = initialCards.map((element) => {
-  return element.name;
-});
-let i = 0;
 
 // Функция создания карточки
-function addCard(link, name, functionFindDeleteButton) {
+function createCard(link, name, cardDeleteCallback) {
   const cardItem = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImage = cardItem.querySelector(".card__image");
+  const cardDeleteButton = cardItem.querySelector(".card__delete-button");
 
-  cardItem.querySelector(".card__image").src = link;
-  cardItem.querySelector(".card__image").alt = name;
+  cardImage.src = link;
+  cardImage.alt = name;
   cardItem.querySelector(".card__title").textContent = name;
 
-  cardContainer.append(cardItem);
-  functionFindDeleteButton(cardItem);
+  cardDeleteButton.addEventListener("click", () =>
+    cardDeleteCallback(cardItem)
+  );
+  return cardItem;
 }
 
 // Функция удаления карточки
-function findDeleteButton(cardItem) {
-  const cardDeleteButton = cardItem.querySelector(".card__delete-button");
-  cardDeleteButton.addEventListener("click", deleteCard);
-
-  function deleteCard() {
-    cardItem.remove();
-  }
+function deleteCard(cardItem) {
+  cardItem.remove();
 }
 
 // Вывести карточки на страницу
-initialCards.forEach(() => {
-  addCard(cardsLinks[i], cardsNames[i], findDeleteButton);
-  i = i + 1;
+initialCards.forEach((initialCards) => {
+  cardContainer.append(
+    createCard(initialCards.link, initialCards.name, deleteCard)
+  );
 });
